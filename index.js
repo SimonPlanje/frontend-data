a301986c583a2ba17667fc684d5945306f6a3e64
Simon Planje <simon.planje2000@gmail.com>
2020-11-12 12:38:35 +0100

    fixed data plotting with new json

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const combineJSON = addIdToLonLat.map((item) => {
 
   //and than as last we filter out the not usable parking spots
   const filterUselessData = filterData(combineJSON)
-  console.log(JSON.stringify(filterUselessData))
+  // console.log(JSON.stringify(filterUselessData))
 
 })
 
@@ -184,6 +184,8 @@ const projection = d3.geoMercator()
 .center([6, 52])                // GPS of location to zoom on
 .scale(5000)                    // This is like the zoom
 const pathGenerator = d3.geoPath().projection(projection)
+//geoPath: this will convert the data path into an svg path string that we can use on svg paths
+//geoMercator: this is the type of projection type
 
 const g = svg.append('g')
 
@@ -217,23 +219,21 @@ g.selectAll('path').data(gemeentes.features)
       })
 
 
-d3.json('https://raw.githubusercontent.com/SimonPlanje/frontend-data/main/frontenddata/onlineData/longLatData.json')
+
+////----PLOTTING THE LON LAT AS CIRCLES ON THE MAP ⬇️⬇️⬇️------
+d3.json('https://raw.githubusercontent.com/SimonPlanje/frontend-data/main/onlineData/longLatDisabled.json')
   .then(data => {
 
     g
     .selectAll('circle').data(data)
     .enter().append('circle')
     .attr('class', 'parkSpots')
-    .attr('cx', d => projection([d.longitude, d.latitude])[0])
-    .attr('cy', d => projection([d.longitude, d.latitude])[1])
+    .attr('cx', d => projection([d.accessPointLocation[0].longitude, d.accessPointLocation[0].latitude])[0])
+    .attr('cy', d => projection([d.accessPointLocation[0].longitude, d.accessPointLocation[0].latitude])[1])
     .attr('r', '2px')
-
     })
-////----PLOTTING THE LON LAT AS CIRCLES ON THE MAP ⬇️⬇️⬇️------
 
 
-//geoPath: this will convert the data path into an svg path string that we can use on svg paths
-//geoMercator: this is the type of projection type
 
 //-----------------------------------------
 //----PLOTTING THE LON LAT ON THE MAP------
