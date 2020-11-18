f35da95ee8e44ea9335e8f5746253643662cfd66
Simon Planje <simon.planje2000@gmail.com>
2020-11-18 10:59:09 +0100

    seprated d3 and fP in 2 files

--- index.js
+++ index.js
@@ -1,234 +1,17 @@
-// 1 Haal de RDW data op	3
-// 2 Selecteer de kolom in de dataset die ik wil onderzoeken/visualiseren	
-// 3 Schoon deze data op	
-
-const endpoint = 'https://raw.githubusercontent.com/SharonV33/frontend-data/main/data/parkeergarages_1000.json'	
-const areaIdColumn = getData;	
-const selectedColumn = 'parkingFacilityInformation';	
-
-async function getData(url){	
-    const response = await fetch(url)	
-    const data = await response.json()	
-
-    return data	
-}	
-
-getData(endpoint).then(RDWData => {	
-
-
-  //get data all data	
-  const allData = filterAccesspoint(RDWData, selectedColumn);	
-
-  // removes all the arrays around the objects	
-  const removeArrays = removeArray(allData);	
-
-  //replace undifined values with null	
-  const emptyFixed = fixEmptyKeys(removeArrays)	
-
-  //filter out the long lat	
-  const longLatArray = getLocationArray(emptyFixed);	
-
-  //Alle null waardes weghalen	
-  const removeNullValues = removeNulls(longLatArray)	
-
-  //Weer een een array om object weghalen	
-  const removeArrayLonLat = removeArray(removeNullValues);	
-
-  // //make array of the long lat of every object	
-  // const geoArray = createLongLatArray(removeArrayLonLat)	
-  // console.log(JSON.stringify(geoArray))	
-
-
-  //Get disabled Data	
-  const disabledArray = filterDisabled(RDWData, selectedColumn)	
-
-  const removeArrayDisabled = removeArray(disabledArray)	
-
-  // const removeObjectsDisabled = removeObjects(removeArrayDisabled)	
-
-  //Calls the function that replaces undefined for {} so i can add and id to the object	
-  const objectArray = addObjectUndef(removeArrays)	
-
-  //adds id's to the lonlat data objects so I can combine it with the other data variable	
-  const addIdToLonLat = addIds(objectArray)	
-
-  //adds id's to the disabled data objects so I can combine it with the other data variable	
-  const addIdToDisabled = addIds(removeArrayDisabled)	
-
-
-const combineJSON = addIdToLonLat.map((item) => {	
-  // console.log('dit is t item: ', item)	
-  return{	
-    ...item,	
-    ...addIdToDisabled.filter(data => data.id === item.id)[0]	
-    //https://flaviocopes.com/how-to-merge-objects-javascript/	
-  }	
-})	
-
-  // console.log('volledige JSON: ', combineJSON)	
-
-  //and than as last we filter out the not usable parking spots	
-  const filterUselessData = filterData(combineJSON)	
-  // console.log(filterUselessData)	
-
-  //Aantal loops om classes toe te voegen aan de dataset	
-  const addsClassesNone = addClassesNone(filterUselessData)	
-  const addsClassesDisabled = addClassesDisabled(addsClassesNone)	
-  const addsClassesCharging = addClassesCharging(addsClassesDisabled)	
-  const addsClassesBoth = addClassesBoth(addsClassesCharging)	
-  // console.log('both: ', JSON.stringify((addsClassesBoth)))	
-
-
-
-})	
-
-
-
-//FUNCTION PART	
-
-//remove data that cant be used	
-function filterData(data){	
-  return data.filter(result => result.accessPointLocation);	
-}	
-
-
-
-function addObjectUndef(data){	
-  return data.map(result => {	
-    if(result == undefined){	
-      return result = {}	
-    }else{	
-      return result	
-    }	
-  })	
-}	
-
-function addIds(data){	
-  return data.map((item, index) => {	
-    if(index !== undefined){	
-      return {...item, id: index + 1}	
-    }else{	
-      return {id: index + 1}	
-    }	
-    //https://stackoverflow.com/questions/50023291/add-id-to-array-of-objects-javascript	
-  })	
-}	
-
-function addClassesNone(data){	
-  return data.map((item) => {	
-    // console.log('item', item)	
-    if(item.chargingPointCapacity == 0 && item.disabledAccess == false){	
-      return {...item, id: 'none'}	
-    }else{	
-      return item	
-    }	
-
-  })	
-}	
-
-function addClassesDisabled(data){	
-  return data.map((item) => {	
-  if(item.chargingPointCapacity == 0 && item.disabledAccess == true){	
-    return {...item, id: 'disabled'}	
-  } else{	
-    return item	
-  }	
-})	
-}	
-
-function addClassesCharging(data){	
-  return data.map((item) => {	
-
-  if(item.chargingPointCapacity > 0 && item.disabledAccess == false){	
-    return {...item, id: 'charging'}	
-  } else{	
-    return item	
-  }	
-})	
-}	
-
-function addClassesBoth(data){	
-  return data.map((item) => {	
-    // console.log('item', item)	
-    if(item.chargingPointCapacity > 0 && item.disabledAccess == true){	
-      return {...item, id: 'both'}	
-    }else{	
-      return item	
-    }	
-
-  })	
-}	
-
-//get disbaled data 	
-function filterDisabled(dataArray, index) {	
-  return dataArray.map(item => item[index].specifications )	
-}	
-
-// function removeObjects(data){	
-//   return data.filter(result => result.chargingPointCapacity !== undefined);	
-// }	
-
-function filterAccesspoint(dataArray, index) {	
-    return dataArray.map(item => item[index].accessPoints )	
-  }	
-
-
-function getLocationArray(data){	
-    return data.map(item => item.accessPointLocation )	
-}	
-
-
-function removeArray(data){	
-    return data.map(result => result[0])	
-}	
-
-
-function removeNulls(data){	
-    return data.filter(result => result !== null);	
-}	
-
-// function createLongLatArray(data){	
-//     let lat = data.map(result => result.latitude )	
-//     let long = data.map(result => result.longitude)	
-//     return lat.map((latitude, index) =>{	
-//         return [latitude, long[index]]}) 	
-// }	
-
-
-function fixEmptyKeys(data){	
-
-    // Create an object with all the keys in it	
-    // This will return one object containing all keys the items	
-    let obj = data.reduce((res, item) => ({...res, ...item}));	
-
-    // Get those keys as an array	
-    let keys = Object.keys(obj);	
-
-    // Create an object with all keys set to the default value null	
-        let def = keys.reduce((result, key) => {	
-        result[key] = null	
-        return result;	
-      }, {});	
-
-      // Use object destrucuring to replace all default values with the ones we have	
-      return data.map((item) => ({...def, ...item}));	
-      //source: https://stackoverflow.com/questions/47870887/how-to-fill-in-missing-keys-in-an-array-of-objects/47871014#47871014?newreg=7adc7a5e48b7436d99619b4aad68d8f8	
-}	
-
-
-//----------------------------------------------------------------------	
-//D3 ---------⬇️⬇️⬇️-------- d3 logic ---------⬇️⬇️⬇️------------- D3	
+//----------------------------------------------------------------------
+//D3 ---------⬇️⬇️⬇️-------- d3 logic ---------⬇️⬇️⬇️------------- D3
 //----------------------------------------------------------------------
 //---- MAKING THE MAP -------
 
-const svg = d3.select('svg');
+const svg = d3.select('svg')
 
-const height = parseFloat(svg.attr('height'));
-const width = +svg.attr('width');
+const height = parseFloat(svg.attr('height'))
+const width = +svg.attr('width')
 
-const projection = d3.geoMercator()
-.center([5, 52.5])                // GPS of location to zoom on
-.scale(8000)                    // This is like the zoom
+const projection = d3
+  .geoMercator()
+  .center([5, 52.5]) // GPS of location to zoom on
+  .scale(8000) // This is like the zoom
 const pathGenerator = d3.geoPath().projection(projection)
 //geoPath: this will convert the data path into an svg path string that we can use on svg paths
 //geoMercator: this is the type of projection type
@@ -236,108 +19,118 @@ const pathGenerator = d3.geoPath().projection(projection)
 const g = svg.append('g')
 var radius = '2px'
 //Bepaal kleur voor circles
-var color = d3.scaleOrdinal()
-    .domain(['none', 'both', 'disabled', 'charging'])
-    .range(['var(--pink2)', 'var(--yellow)', 'var(--orange)', 'var(--lime)'])
-
+var color = d3
+  .scaleOrdinal()
+  .domain(['none', 'both', 'disabled', 'charging'])
+  .range(['var(--pink2)', 'var(--yellow)', 'var(--orange)', 'var(--lime)'])
 
 //source: https://www.d3-graph-gallery.com/graph/bubblemap_buttonControl.html
 
-//ZOOMEN 
-svg.call(d3.zoom()
-  .on('zoom', zoomed));
+//ZOOMEN
+svg.call(d3.zoom().on('zoom', zoomed))
 
-function zoomed({transform}) {
-g.attr('transform', transform);
+function zoomed({ transform }) {
+  g.attr('transform', transform)
 }
 //source: https://www.youtube.com/watch?v=9ZB1EgaJnBU&t=904s
 
-
 g.append('path')
-.attr('class', 'sphere')
-.attr('d', pathGenerator({type: 'Sphere'}));
+  .attr('class', 'sphere')
+  .attr('d', pathGenerator({ type: 'Sphere' }))
 
+d3.json('https://cartomap.github.io/nl/wgs84/gemeente_2020.topojson').then(
+  (data) => {
+    const gemeentes = topojson.feature(data, data.objects.gemeente_2020)
 
-d3.json('https://cartomap.github.io/nl/wgs84/gemeente_2020.topojson')
-.then(data => {
-
-  const gemeentes = topojson.feature(data, data.objects.gemeente_2020 )
-
-g.selectAll('path').data(gemeentes.features)
-    .enter().append('path')
+    g.selectAll('path')
+      .data(gemeentes.features)
+      .enter()
+      .append('path')
       .attr('class', 'gemeente')
       .attr('d', pathGenerator)
-    .append('title')
-      .text(d => d.properties.statnaam)
+      .append('title')
+      .text((d) => d.properties.statnaam)
+  }
+)
 
-      })
-
-
-
-////----PLOTTING THE LON LAT AS CIRCLES ON THE MAP ⬇️⬇️⬇️------
-d3.json('https://raw.githubusercontent.com/SimonPlanje/frontend-data/main/onlineData/longLatDisabled.json')
-  .then(data => {
-
-    g
-    .selectAll('circle').data(data)
-    .enter().append('circle')
-    .attr('class', d => d.id )
-    .attr('cx', d => projection([d.accessPointLocation[0].longitude, d.accessPointLocation[0].latitude])[0])
-    .attr('cy', d => projection([d.accessPointLocation[0].longitude, d.accessPointLocation[0].latitude])[1])
+//----PLOTTING THE LON LAT AS CIRCLES ON THE MAP ⬇️⬇️⬇️------
+d3.json(
+  'https://raw.githubusercontent.com/SimonPlanje/frontend-data/main/onlineData/longLatDisabled.json'
+).then((data) => {
+  g.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', (d) => d.id)
+    .attr(
+      'cx',
+      (d) =>
+        projection([
+          d.accessPointLocation[0].longitude,
+          d.accessPointLocation[0].latitude,
+        ])[0]
+    )
+    .attr(
+      'cy',
+      (d) =>
+        projection([
+          d.accessPointLocation[0].longitude,
+          d.accessPointLocation[0].latitude,
+        ])[1]
+    )
     .attr('r', radius)
-    .attr('fill', d => color(d.id))
-    .attr('stroke', d => color(d.id))
-    .attr('fill-opacity', .3)
-    //update function for the checkboxes
+    .attr('fill', (d) => color(d.id))
+    .attr('stroke', (d) => color(d.id))
+    .attr('fill-opacity', 0.3)
+  //update function for the checkboxes
 
-function update(){
-  // For each check box:
-  d3.selectAll('.checkbox').each(function(d){
-    cb = d3.select(this);
-    group = cb.property('value')
+  function update() {
+    // For each check box:
+    d3.selectAll('.checkbox').each(function (d) {
+      cb = d3.select(this)
+      group = cb.property('value')
 
-    // If the box is check, I show the group
-    if(cb.property('checked')){
-      g.selectAll('.'+group)
-      .transition()
-      .duration(500)
-      .style('opacity', 1)
-      .attr('r', radius)
+      // If the box is check, I show the group
+      if (cb.property('checked')) {
+        g.selectAll('.' + group)
+          .transition()
+          .duration(500)
+          .style('opacity', 1)
+          .attr('r', radius)
 
-    // Otherwise I hide it
-    }else{
-      g.selectAll('.'+group)
-      .transition()
-      .duration(500)
-      .style('opacity', 0)
-      .attr('r', 0)
-    }
-  })
-}
-d3.selectAll('.checkbox').on('change', update)
-
-update()
-
+        // Otherwise I hide it
+      } else {
+        g.selectAll('.' + group)
+          .transition()
+          .duration(500)
+          .style('opacity', 0)
+          .attr('r', radius)
+      }
     })
+  }
+  d3.selectAll('.checkbox').on('change', update)
 
+  update()
+})
 
 //legend
-svg.selectAll('svg')
+svg
+  .selectAll('svg')
   .data(color.domain())
-  .enter().append('circle')
-    .attr('transform', (d, i) =>
-        `translate(${100},${i * 30 + 20})`)
-    .attr('r', 6)
-    .attr('fill', d => color(d))
-    .attr('stroke', d => color(d))
-    .attr('fill-opacity', .3)
+  .enter()
+  .append('circle')
+  .attr('transform', (d, i) => `translate(${100},${i * 30 + 20})`)
+  .attr('r', 6)
+  .attr('fill', (d) => color(d))
+  .attr('stroke', (d) => color(d))
+  .attr('fill-opacity', 0.3)
 
-svg.selectAll('svg')
+svg
+  .selectAll('svg')
   .data(color.domain())
   .enter()
   .append('text')
-    .text(d => d)
-    .style('fill', 'white')
-    .attr('alignment-baseline','middle')
-    .attr('transform', (d, i) =>
-    `translate(${120},${i * 30 + 20})`)
+  .text((d) => d)
+  .style('fill', 'white')
+  .attr('alignment-baseline', 'middle')
+  .attr('transform', (d, i) => `translate(${120},${i * 30 + 20})`)
